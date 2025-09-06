e9172c7fb87b65d97c2ece4f3c18612148e611a9
Vitor Neves <vitorneves.web@gmail.com>
2025-09-05 22:28:46 -0300

    Atualização

--- js/menu_mobile.js
+++ js/menu_mobile.js
@@ -1,5 +1,5 @@
 const btnAbrirMenuMobile = document.querySelector('#btn-menu-mobile');
-const menuMobile = document.querySelector('#menu_mobile');
+const menuMobile = document.querySelector('#menu-mobile');
 
 btnAbrirMenuMobile.addEventListener('click',()=>{
     menuMobile.classList.add('abrir-menu');
